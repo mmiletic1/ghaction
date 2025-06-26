816e257248da0cce9bc1aa5ce6bd1750792dabc3
mmiletic1 <milos.miletic@tenderly.co>
2025-06-26 16:03:03 +0200

    without any if

--- install-linux.sh
+++ install-linux.sh
@@ -1,48 +1,35 @@
 #!/bin/bash
 
-set -euo pipefail
-
 CUR_VERSION=""
-TENDERLY_BIN="tenderly"
-TARBALL="tenderly-binary.tar.gz"
-TMP_DIR="/tmp/tenderly-cli-update"
-LATEST_API="https://api.github.com/repos/Tenderly/tenderly-cli/releases/latest"
+NEW_VERSION="$(curl -s https://api.github.com/repos/Tenderly/tenderly-cli/releases/latest | grep tag_name | cut -d'v' -f2 | cut -d'"' -f1)"
+EXISTS="$(command -v tenderly)"
 
-# Get latest version
-NEW_VERSION=$(curl -s $LATEST_API | grep '"tag_name":' | cut -d'v' -f2 | cut -d'"' -f1)
+printf "Installing version %s\n" $NEW_VERSION
 
-# Check if Tenderly is installed
-if command -v $TENDERLY_BIN &> /dev/null; then
-  CUR_VERSION=$($TENDERLY_BIN version | sed -n 1p | cut -d'v' -f3)
-  echo -e "\nCurrent Version: $CUR_VERSION => New Version: $NEW_VERSION"
-else
-  echo -e "\nTenderly CLI not found. Will install fresh version $NEW_VERSION."
-fi
+cd /tmp/ > /dev/null
 
-# Compare and install if needed
-if [ "$NEW_VERSION" != "$CUR_VERSION" ]; then
-  echo "Installing version $NEW_VERSION..."
+tarball="tenderly-binary.tar.gz"
 
-  mkdir -p $TMP_DIR
-  cd $TMP_DIR > /dev/null
+curl -s https://api.github.com/repos/Tenderly/tenderly-cli/releases/latest \
+| grep "browser_download_url.*Linux_amd64\.tar\.gz" \
+| cut -d ":" -f 2,3 \
+| tr -d \" \
+| xargs curl -sLo $tarball
 
-  # Download tarball
-  curl -s $LATEST_API \
-    | grep "browser_download_url.*Linux_amd64\.tar\.gz" \
-    | cut -d ":" -f2,3 \
-    | tr -d \" \
-    | xargs curl -sLo $TARBALL
+tar -xzf $tarball
 
-  # Extract and move binary
-  tar -xzf $TARBALL
-  chmod +x $TENDERLY_BIN
-  sudo mv $TENDERLY_BIN /usr/local/bin/
+chmod +x tenderly
 
-  echo "Tenderly CLI installed to: $(which $TENDERLY_BIN)"
-  echo "New Tenderly version installed: $($TENDERLY_BIN version | sed -n 1p | cut -d'v' -f3)"
+unlink $tarball
 
-  cd - > /dev/null
-  rm -rf $TMP_DIR
-else
-  echo "Latest version already installed."
-fi
+printf "Moving CLI to /usr/local/bin/\n"
+
+mv tenderly /usr/local/bin/
+
+cd - > /dev/null
+
+location="$(which tenderly)"
+printf "Tenderly CLI installed to: %s\n" $location
+
+version="$(tenderly version | sed -n 1p | cut -d'v' -f3)"
+printf "New Tenderly version installed: %s\n" $version
